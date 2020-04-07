776719364c9581f534e88216594ff592b7f7bed8
Mikael Grön <skaramicke@gmail.com>
2020-04-07 08:49:36 +0200

    remove daemon crap that wasn't working properly

--- duet3d-monitor.py
+++ duet3d-monitor.py
@@ -1,12 +1,11 @@
 #!./env/bin/python
 
-from daemon import Daemon
 import os, sys, time, logging, json, random, requests
 import google
 import googlesay
 import configparser
 
-logging.basicConfig(format='%(asctime)s: %(levelname)s %(message)s', filename="runtime.log", level=logging.WARN)
+logging.basicConfig(format='%(asctime)s: %(levelname)s %(message)s', filename="runtime.log", level=logging.INFO)
 logger = logging.getLogger()
 logger.info('Logging started')
 
@@ -36,47 +35,27 @@ def tell(message: str):
         googlesay.say(message)
 
 
-class NotifierDaemon(Daemon):
-    def run(self):
-        while True:
+def tick(self = None):
+    status_filename = '/tmp/duet3d_last_status.txt'
 
-            status_filename = '/tmp/duet3d_last_status.%s.txt' % self.pid()
-            status_filename = '/tmp/duet3d_last_status.txt'
+    previous_status = ''
+    with open(status_filename, 'r') as previous:
+        previous_status = previous.readline()
 
-            previous_status = ''
-            with open(status_filename, 'r') as previous:
-                previous_status = previous.readline()
+    current_status = check()
 
-            current_status = check()
+    if current_status != previous_status:
+        logger.info('Status changed from %s to %s' % (previous_status, current_status))
+        
+        with open(status_filename, 'w+') as previous:
+            previous.write(current_status)
+        
+        tell(current_status)
 
-            if current_status != previous_status:
-                logger.info('Status changed from %s to %s' % (previous_status, current_status))
-                
-                with open(status_filename, 'w+') as previous:
-                    previous.write(current_status)
-                
-                tell(current_status)
-
-
-            time.sleep(float(config.get('API', 'delay_seconds').replace(',','.'))) 
+    time.sleep(float(config.get('API', 'delay_seconds').replace(',','.'))) 
 
 
 if __name__ == "__main__":
-    daemon = NotifierDaemon('/tmp/duet3d-monitor.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            logger.info("Daemon starting")
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-            logger.info("Daemon stopped")
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-            logger.info("Daemon restarted")
-        else:
-            print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print("usage: %s start|stop|restart" % sys.argv[0])
-        sys.exit(2)
+    print("Running")
+    while True:
+        tick()
